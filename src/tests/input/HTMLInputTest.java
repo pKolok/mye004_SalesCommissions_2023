d67d890985ba50dea3a567bc7c98c1ca689877d9
panousias <panousias@hotmail.com>
2023-11-13 20:03:51 +0200

    Refactoring: Testing: Reviewed & updated input package tests

--- src/tests/input/HTMLInputTest.java
+++ src/tests/input/HTMLInputTest.java
@@ -2,25 +2,26 @@ package tests.input;
 
 import static org.junit.Assert.*;
 
-import data.Representative;
-import data.SaleItem;
-import input.XMLInput;
-
 import java.io.File;
+
 import org.junit.Test;
 
-public class XMLInputTest {
+import data.Representative;
+import data.SaleItem;
+import input.HTMLInput;
+
+public class HTMLInputTest {
 
 	@Test
 	public void testReadFileOneReceiptHappyDay() {
 		File file = new File(
-				"src/tests/input/test_input_files/xml_1Receipt.xml");
-		XMLInput xmlInput = new XMLInput(file);
+				"src/tests/input/test_input_files/html_1Receipt.html");
+		HTMLInput xmlInput = new HTMLInput(file);
 		xmlInput.readFile();
 			
 		Representative agent = xmlInput.getSalesRepresentative();
 		
-		assertEquals(agent.getName(), "Vassileios Zarras");
+		assertEquals(agent.getName(), "Panagiotis");
 		assertEquals(agent.getAfm(), "130456097");
 		
 		assertEquals(agent.calculateTotalSales(), 2000, 0.01);
@@ -31,17 +32,17 @@ public class XMLInputTest {
 	@Test
 	public void testReadFileFourReceiptsHappyDay() {
 		File file = new File(
-			"src/tests/input/test_input_files/xml_4Receipts.xml");
-		XMLInput xmlInput = new XMLInput(file);
+			"src/tests/input/test_input_files/html_4Receipts.html");
+		HTMLInput xmlInput = new HTMLInput(file);
 		xmlInput.readFile();
 		
 		Representative agent = xmlInput.getSalesRepresentative();
 		
-		assertEquals(agent.getName(), "Vassileios Zarras");
+		assertEquals(agent.getName(), "Panagiotis");
 		assertEquals(agent.getAfm(), "130456097");
 
 		assertEquals(agent.calculateTotalSales(), 12000, 0.01);
-		assertEquals(agent.calculateTotalItems(), 85);
+		assertEquals(agent.calculateTotalItems(), 40);
 		assertEquals(agent.calculateItemSales(SaleItem.COAT), 2000, 0.01);
 		assertEquals(agent.calculateItemSales(SaleItem.SHIRT), 4000, 0.01);
 		assertEquals(agent.calculateItemSales(SaleItem.SKIRT), 1000, 0.01);
